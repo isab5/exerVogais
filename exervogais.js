5e531039e9dda86497d615312dca0ba87aa2690d
Isabella <isabellaborin35@gmail.com>
2024-03-19 09:47:43 -0300

    Atividade feita

--- exervogais.js
+++ exervogais.js
@@ -1,1 +1,19 @@
-console.log ("vogais")
+let palavra = "Otorrinolaringologista";
+
+let vogais = ['a', 'e', 'i', 'o', 'u'];
+
+let palavraMinuscula = palavra.toLowerCase();
+
+let caracteres = palavraMinuscula.split('');
+
+let vogaisEncontradas = caracteres.filter(letra => vogais.includes(letra));
+
+let numeroVogais = palavra;
+
+let numero = vogaisEncontradas.length;
+
+console.log("Texto: " + palavra);
+
+console.log("Número totais de vogais: " + numero);
+
+console.log("Vogais encontradas: " + vogaisEncontradas.join(", "));
